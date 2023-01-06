364464cf07786e41a676e3766663ba889b92d5ca
colloso999 <tripperskripper@gmail.com>
2023-01-07 00:37:43 +0300

    modified 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,6 @@
 
 # a function that replaces an element of a list at a specific position
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
     return my_list
