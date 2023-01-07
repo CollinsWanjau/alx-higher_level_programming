d0b81463b7f6616358eb2c2b94d36bd51bdecc84
colloso999 <tripperskripper@gmail.com>
2023-01-07 10:36:00 +0300

    modified .gitignore

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# a function that replaces an element in a list at a specific position
+# without modifying the original list
+def new_in_list(my_list, idx, element):
+
+    # If idx is negative, the function should return a copy of the
+    # original list
+    # If idx is out of range (> of number of element in my_list), the function
+    # should return a copy of the original list
+    if idx < 0 and idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
