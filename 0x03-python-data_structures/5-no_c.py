ca0c6adb3ea95c103e505fba50eb2a966676e3f7
colloso999 <tripperskripper@gmail.com>
2023-01-07 12:48:23 +0300

    modified 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# the function should return the new string
+def no_c(my_string):
+    for i in my_string:
+        if (i == "c") or (i == "C"):
+            my_string = my_string[:my_string.index(i)] \
+                    + my_string[my_string.index(i) + 1:]
+    return my_string
+
+
