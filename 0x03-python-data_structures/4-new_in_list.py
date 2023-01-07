f25c3b44367969e2c1dd254fd4838f5ff9d0b82f
colloso999 <tripperskripper@gmail.com>
2023-01-07 10:43:30 +0300

    modified 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,7 +8,7 @@ def new_in_list(my_list, idx, element):
     # original list
     # If idx is out of range (> of number of element in my_list), the function
     # should return a copy of the original list
-    if idx < 0 and idx >= len(my_list):
+    if (idx < 0) or (idx >= len(my_list):)
         return my_list[:]
     new_list = my_list[:]
     new_list[idx] = element
