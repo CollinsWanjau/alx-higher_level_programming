0930ab3d5845ced9f56e99536204ab8b171d90ff
colloso999 <tripperskripper@gmail.com>
2023-01-09 19:39:36 +0300

    modified 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-# a function that returns a tuple with the length of a string and its first character
+# a function that returns a tuple with the length of a string and its
+# first character
 def multiple_returns(sentence):
     length = len(sentence)
     # If the sentence is empty, the first character should be equal to None
@@ -11,4 +12,3 @@ def multiple_returns(sentence):
     tpl = (len(sentence), first)
 
     return tpl
-
