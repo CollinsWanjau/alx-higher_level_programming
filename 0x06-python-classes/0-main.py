15a2addff1a0a1e938d26de6b63a5b6654574512
colloso999 <tripperskripper@gmail.com>
2023-01-30 22:38:26 +0300

    created folder class

---
+++ 0x06-python-classes/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+Square = __import__(0-square).Square
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
+
+"""
+__dict__ attribute is a dictionary that contains all attributes (instance
+variables) of an object
+"""
