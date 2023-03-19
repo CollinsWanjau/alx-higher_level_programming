80486d707d9cb6bea20aa5fee0ebb101028a870a
colloso999 <tripperskripper@gmail.com>
2023-03-19 02:35:00 +0000

    Filters states where name matches the arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in an arg and displays all 
+Takes in an arg and displays all
 values in the the state table where n
 ame matches args
 """
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     Access the database and get the states
     from the database
     """
-    conn  = db.connect(host="localhost", port=3306, user=argv[1],
+    conn = db.connect(host="localhost", port=3306, user=argv[1],
                       passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
