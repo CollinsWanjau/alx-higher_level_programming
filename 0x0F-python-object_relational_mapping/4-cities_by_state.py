bd95f32294a16b2330b8f4da2e463007c22150ec
colloso999 <tripperskripper@gmail.com>
2023-05-26 18:39:56 +0300

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,5 +6,12 @@ Lists all cities in ASC order
 if __name__ == '__main__':
     import MySQLdb as db
     from sys import argv
-    conn = db.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    query = ""
+    conn = db.connect(host="localhost", port=3306, user=argv[1],
+                      passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON\
+            state_id = states.id ORDER BY cities.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
